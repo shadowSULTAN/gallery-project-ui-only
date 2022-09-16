0a29a4bf92d69e31c97810fecc8e18e3990d2081
Akhil Surendran <akhilsurendranvr@gmail.com>
2022-09-16 12:33:58 +0530

    Initial Commit

---
+++ gallery project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gallery project
+//
+//  Created by ddukk15 on 14/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
